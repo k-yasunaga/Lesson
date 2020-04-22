e58369f1d8efbc8c39cfe3ab5395890f735babad
edu08 <edu08@EDUPC08.n00d01.kis.co.jp>
2020-04-22 14:21:02 +0900

    ensyu

--- src/lesson/Main.java
+++ src/lesson/Main.java
@@ -5,7 +5,27 @@ public class Main {
 	public static void main(String[] args) {
 		double p = 3.14;
 		System.out.println("円周率は" + p + "です。");
+			int a ;
+			int b ;
+			a = 20;
+			b = a+5;
+			System.out.println(a);
+			System.out.println(b);
+			System.out.println("今日は\"4月\"です");
+			System.out.println("\\100");
+			System.out.println("C:\\に\"フォルダ\"を作成");
+			int num =10;
+			int amari  = num % 3;
+			System.out.println(amari);
+			num +=5;
+			System.out.println(num);
+			double d = 3;
+			int n = (int)3.0;
+			String s = "ベスト" + 3;
 
+			int kokugo = 70;
+			int sansu = 75;
+			System.out.println((double)(kokugo + sansu) / 2);
 	}
 
 }
