a0f56ca9bedcfd7ce8630d38b67577b3bea8509c
edu08 <edu08@EDUPC08.n00d01.kis.co.jp>
2020-04-22 16:27:34 +0900

    演習課題

--- src/lesson/kuku.java
+++ src/lesson/kuku.java
@@ -3,17 +3,17 @@ package lesson;
 public class kuku {
 
 	public static void main(String[] args) {
-		int a = new java.util.Random().nextInt(9)+1;
-		int b = new java.util.Random().nextInt(9)+1;
-		int num = a*b;
-		System.out.println(a + "×" + b +"は？");
+		int num1 = new java.util.Random().nextInt(9)+1;
+		int num2 = new java.util.Random().nextInt(9)+1;
+		int seki= num1 * num2;
+		System.out.println(num1 + "×" + num2 +"は？");
 		System.out.println("答えを入力してください");
-		int c = new java.util.Scanner(System.in).nextInt();
-		 if(c==num) {
+		int kotae = new java.util.Scanner(System.in).nextInt();
+		 if(seki== kotae) {
 			 System.out.println("正解です");
 		 }
 		 else {
-			 System.out.println("間違いです。正解は" + num + "です。");
+			 System.out.println("間違いです。正解は" + seki + "です。");
 		 }
 	}
 
