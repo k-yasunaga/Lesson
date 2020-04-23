1f6b307fc2e0a0286b6ad7ce66d32d51f7ed215c
edu08 <edu08@EDUPC08.n00d01.kis.co.jp>
2020-04-23 16:38:33 +0900

    拡張forに変更

--- src/lesson/Coin.java
+++ src/lesson/Coin.java
@@ -6,10 +6,10 @@ public class Coin {
 		int[] coins = {500,100,50,10,5,1};
 		int coin ;
 		int sum= 0;
-		for(int i= 0;i<coins.length;i++) {
-		System.out.println( coins[i]+ "円玉は何枚？");
+		for(int C:coins) {
+		System.out.println( C+ "円玉は何枚？");
 		coin = new java.util.Scanner(System.in).nextInt();
-		 sum += coin * coins[i];
+		 sum += coin * C;
 		}
 		System.out.println(sum + "円");
 
