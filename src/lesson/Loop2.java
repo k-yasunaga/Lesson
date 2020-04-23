17dcf45abac335e924252b54661de2625b919766
edu08 <edu08@EDUPC08.n00d01.kis.co.jp>
2020-04-23 11:50:15 +0900

    1～10合計

--- src/lesson/Loop2.java
+++ src/lesson/Loop2.java
@@ -31,6 +31,13 @@ public class Loop2 {
 		for(int i= 5;i>0;i--) {
 			System.out.println(i);
 		}
+		System.out.println();//改行
+	//6.
+		int sum =0;
+		for(int i =1;i<=10;i++) {
+			sum+=i;
+		}
+		System.out.println(sum);
 	}
 
 }
