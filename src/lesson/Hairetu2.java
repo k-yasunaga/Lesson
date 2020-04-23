995b11f429cededa227cb3f89c067528dc5c29e8
edu08 <edu08@EDUPC08.n00d01.kis.co.jp>
2020-04-23 15:58:01 +0900

    演習問題4/23

--- src/lesson/Hairetu2.java
+++ src/lesson/Hairetu2.java
@@ -4,10 +4,14 @@ public class Hairetu2 {
 
 	public static void main(String[] args) {
 
-		int[] data = {5,8,7,9,6};
-		for(int D:data) {
-			System.out.println(D);
-		}
+//		int[] data = {5,8,7,9,6};
+//		for(int D:data) {
+//			System.out.println(D);
+//		}
+
+
+
+
 
 //     	int[] prices = {100,200,150};
 //		for(int i = 0;i<prices.length;i++) {
@@ -19,10 +23,10 @@ public class Hairetu2 {
 //		for(int value:prices) {
 //			System.out.println(value);
 //		}
-//		String[] names = {"コーラ","コーヒー","水"};
-//		for(String value:names) {
-//			System.out.println(value);
-//		}
+		String[] names = {"コーラ","コーヒー","水"};
+		for(String str:names) {
+			System.out.println(str + "は" +str.length() + "文字");
+		}
 //
 //		int[] points = {80,90,60,70,50};
 //		int sum = 0;
---
+++ src/lesson/Hairetu3.java
@@ -0,0 +1,37 @@
+package lesson;
+
+public class Hairetu3 {
+
+	public static void main(String[] args) {
+		int[] nums = {8,5,9,2,4};
+		for(int N:nums) {
+		System.out.println(N);
+		}
+			System.out.println();//改行
+
+		int sum =0;
+		for(int N:nums) {
+			sum+=N;
+		}
+		System.out.println(sum);
+			System.out.println();//改行
+
+	 int count =0;
+		for(int N:nums) {
+			if(N>=5) {
+				count++;
+			}
+		}
+		System.out.println(count);
+			System.out.println();//改行
+
+		int max =0;
+		for(int i= 0;i<5;i++) {
+			nums[i] *=2;
+		   if(nums[i]>max) {
+			   max = nums[i];
+		   }
+		}
+		System.out.println(max);
+	}
+}
