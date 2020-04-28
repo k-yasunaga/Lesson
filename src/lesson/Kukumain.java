6160f3ed3bb34c135f2f63c719ec3bd0e2057e1b
edu08 <edu08@EDUPC08.n00d01.kis.co.jp>
2020-04-28 16:13:52 +0900

    九九

--- src/lesson/Kukumain.java
+++ src/lesson/Kukumain.java
@@ -3,7 +3,7 @@ package lesson;
 import java.util.ArrayList;
 
 public class Kukumain {
-	Kukumondai K= new Kukumondai();
+
 	public static void main(String[] args) {
 		ArrayList<Kukumondai> klist = new ArrayList<Kukumondai>();
 		klist.add(new Kukumondai());
